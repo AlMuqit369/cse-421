f437ef28c1dbf5723f637ef8fb8bb4d7eb5daa65
AlMuqit369 <almuqit369@gmail.com>
2026-06-26 20:21:06 +0600

    CSE 421 Lab 1

---
+++ Lab 1/task 1/server.py
@@ -0,0 +1,32 @@
+import socket 
+FORMAT='utf-8'
+HEADER= 64
+
+SERVER= socket.gethostbyname(socket.gethostname())
+PORT=4949
+ADDR=(SERVER,PORT)
+DISCONNECT="End"
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+print(f"Server is running on {SERVER}:{PORT}")
+
+server.listen()
+print(f"Server is listen on {SERVER}:{PORT}")
+
+while True:
+    conn,addr=server.accept()
+    print(f"connection from {addr} has been established")
+    connected= True
+    while connected:
+        msg_length=conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length=int(msg_length)
+            msg=conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT:
+                connected=False
+                conn.send('Connection closed'.encode(FORMAT))
+            else:
+                print(msg)
+                conn.send('Message received'.encode(FORMAT))
+    conn.close()
