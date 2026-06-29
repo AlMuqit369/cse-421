8abe48e25de127f48519ce6116045f543cd77c68
AlMuqit369 <almuqit369@gmail.com>
2026-06-29 19:53:53 +0600

    Lab 1 Task 2 done

--- Lab 1/task 2/client.py
+++ Lab 1/task 2/client.py
@@ -1,27 +1,33 @@
-import socket 
+import socket
 
-FORMAT='utf-8'
-HEADER= 64
+FORMAT = 'utf-8'
+HEADER = 64
 
-SERVER= socket.gethostbyname(socket.gethostname())
-PORT=4949
-ADDR=(SERVER,PORT)
-DISCONNECT="End"
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 4949
+ADDR = (SERVER, PORT)
+DISCONNECT = "End"
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def send(msg):
-    message=msg.encode(FORMAT)
-    msg_length=len(message)
-    send_length=str(msg_length).encode(FORMAT)
-    send_length+=b' '*(HEADER-len(send_length))
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
+    print("Server:", client.recv(2048).decode(FORMAT))
 
-    print(client.recv(2048).decode(FORMAT))
 while True:
     msg=input("Enter a message: ")
     send(msg)
     if msg==DISCONNECT:
         break
+
+
+msg = input("Enter a message: ")
+send(msg)
+
+send(DISCONNECT)
--- Lab 1/task 2/server.py
+++ Lab 1/task 2/server.py
@@ -1,42 +1,44 @@
-import socket 
-FORMAT='utf-8'
-HEADER= 64
+import socket
 
-SERVER= socket.gethostbyname(socket.gethostname())
-PORT=4949
-ADDR=(SERVER,PORT)
-DISCONNECT="End"
+FORMAT = 'utf-8'
+HEADER = 64
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 4949
+ADDR = (SERVER, PORT)
+DISCONNECT = "End"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 print(f"Server is running on {SERVER}:{PORT}")
-
 server.listen()
 print(f"Server is listen on {SERVER}:{PORT}")
 
 while True:
-    conn,addr=server.accept()
-    print(f"connection from {addr} has been established")
-    connected= True
+    conn, addr = server.accept()
+    print(f"Connection established from {addr}")
+    connected = True
+
     while connected:
-        msg_length=conn.recv(HEADER).decode(FORMAT)
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+
         if msg_length:
-            msg_length=int(msg_length)
-            msg=conn.recv(msg_length).decode(FORMAT)
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT:
-                connected=False
-                conn.send('Connection closed'.encode(FORMAT))
+                connected = False
+                conn.send("Connection Closed".encode(FORMAT))
+
             else:
-                vowels="AEIOUaeiou"
-                count=0
-                for char in msg:
-                    if char in vowels:
-                        count+=1
-                if count == 0:
+                print("Client Message:", msg)
+                vowels = 0
+                for ch in msg.lower():
+                    if ch in "aeiou":
+                        vowels += 1
+                if vowels == 0:
                     conn.send("Not enough vowels".encode(FORMAT))
-                elif count<=2:
+                elif vowels <= 2:
                     conn.send("Enough vowels I guess".encode(FORMAT))
                 else:
                     conn.send("Too many vowels".encode(FORMAT))
-        
     conn.close()
